03d6489858ed2c5d6261237f7c66d16fdd94ad2c
Changoluisa90 <edwinchangoluisa@hotmail.com>
2023-07-21 14:22:35 -0500

    mi tercer commit

--- admin/secciones/usuarios/crear.php
+++ admin/secciones/usuarios/crear.php
@@ -1,3 +1,63 @@
-<?php include("../../templates/header.php");?>
-crear usuario 
-<?php include("../../templates/footer.php");?>
+<?php
+include("../../bd.php");
+// crear registro 
+if ($_POST) {
+  $usuario=(isset($_POST['usuario']))?$_POST['usuario']:"";
+  $password=(isset($_POST['password']))?$_POST['password']:"";
+  $correo=(isset($_POST['correo']))?$_POST['correo']:"";
+
+  $sentencia=$conexion->prepare("INSERT INTO `usuarios` (`usu_id`,`usu_usuario`,`usu_password`,`usu_correo`)
+    VALUES (NULL, :usuario, :password, :correo);");
+    $sentencia->bindParam(":usuario",$usuario);
+    $sentencia->bindParam(":password",$password);
+    $sentencia->bindParam(":correo", $correo);
+    $sentencia->execute();
+
+   $mensaje="Registro agregado con éxito...........";
+    header("location:index.php?mensaje=",$mensaje);
+}
+
+include("../../templates/header.php");
+
+?>
+crear usuario
+<div class="card">
+    <div class="card-header">
+        crear usuarios
+    </div>
+    <div class="card-body">
+    <form action="" method="post">
+        <div class="mb-3">
+          <label for="usuario" class="form-label">Usuario</label>
+          <input type="text"
+            class="form-control" name="usuario" id="usuario" aria-describedby="helpId" placeholder="Nombre Usuario">
+          
+        </div>
+
+        <div class="mb-3">
+          <label for="password" class="form-label">Password</label>
+          <input type="password"
+            class="form-control" name="password" id="password" aria-describedby="helpId" placeholder="password">
+          
+        </div>
+
+        <div class="mb-3">
+          <label for="correo" class="form-label">Correo</label>
+          <input type="email"
+            class="form-control" name="correo" id="correo" aria-describedby="helpId" placeholder="correo">
+            
+        </div>
+        
+        <button type="submit" class="btn btn-success">Guardar</button>
+        <a name="" id="" class="btn btn-primary" href="index.php" role="button">Cancelar</a>
+        
+
+</form>
+    </div>
+    <div class="card-footer text-muted">
+        
+    </div>
+</div>
+
+
+<?php include("../../templates/footer.php"); ?>
--- admin/secciones/usuarios/index.php
+++ admin/secciones/usuarios/index.php
@@ -1,3 +1,69 @@
-<?php include("../../templates/header.php");?>
-Listar usuario 
-<?php include("../../templates/footer.php");?>
+<?php
+include("../../bd.php");
+include("../../templates/header.php");
+
+if (isset($_GET['txtID'])) {
+
+    //2BORRAR  DICHO REGISTRO CON EL ID CORRESPONDIENTE desde el formulario
+    $txtID=(isset($_GET['txtID']) )?$_GET['txtID']:"";
+    $sentencia=$conexion->prepare("DELETE FROM usuarios WHERE usu_id=:usu_id");
+    $sentencia->bindParam(":usu_id",$txtID);
+    $sentencia->execute();
+}
+
+
+//1seleccionar registros baase de datos
+$sentencia = $conexion->prepare("SELECT * FROM `usuarios` ");
+$sentencia->execute();
+$lista_usuarios = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<div class="card">
+    <div class="card-header">
+    <a name="" id="" class="btn btn-primary" href="crear.php" role="button">Agregar Registros</a>
+    </div>
+    <div class="card-body">
+    <div class="table-responsive-sm">
+    <table class="table table">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Usuario</th>
+                <th scope="col">Contraseña </th>
+                <th scope="col">Correo </th>
+                <th scope="col">Acciones </th>
+
+            </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($lista_usuarios as $registros) { ?>
+                        <tr class="">
+                            <td><?php echo $registros['usu_id']; ?></td>
+                            <td><?php echo $registros ['usu_usuario'] ?></td>
+                            <td><?php echo $registros ['usu_password'] ?></td>
+                            <td><?php echo $registros ['usu_correo'] ?></td>
+
+                            <td>
+                                <a name="" id="" class="btn btn-info" href="editar.php?txtID=<?php echo $registros['usu_id']?>" role="button"" role="button">Editar</a>
+                                |
+                                <a name="" id="" class="btn btn-danger" href="index.php?txtID=<?php echo $registros['usu_id']?>" role="button">Eliminar</a>
+                                
+                            
+                            </td>
+                        </tr>
+                    <?php } ?>
+        </tbody>
+    </table>
+</div>
+
+    </div>
+    <div class="card-footer text-muted">
+       
+    </div>
+</div>
+
+
+
+
+
+<?php include("../../templates/footer.php"); ?>
