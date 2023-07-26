2c297432265edd8a3b7a32bad156a7f2db04d9c3
Changoluisa90 <edwinchangoluisa@hotmail.com>
2023-07-26 14:10:00 -0500

    mi 5  commit

--- admin/secciones/configuraciones/crear.php
+++ admin/secciones/configuraciones/crear.php
@@ -1,3 +1,52 @@
-<?php include("../../templates/header.php");?>
-crear Configuracíon 
-<?php include("../../templates/footer.php");?>
+<?php
+include("../../bd.php");
+
+
+if ($_POST) {
+
+    //Recepcionamos los valores del formulario
+    $nombreconfiguracion = (isset($_POST['nombreconfiguracion']['name'])) ? $_POST['nombreconfiguracion']['name'] : "";
+    $valor = (isset($_POST['valor'])) ? $_POST['valor'] : "";
+
+    $sentencia = $conexion->prepare("INSERT INTO `configuraciones` (`con_id`,`con_Nombreconfiguracion`,`valor`)
+        VALUES (NULL, :nombreconfiguracion, :valor);");
+    $sentencia->bindParam(":nombreconfiguracion", $nombreconfiguracion);
+    $sentencia->bindParam(":valor", $valor);
+    $sentencia->execute();
+
+    $mensaje = "Registro agregado con éxito...........";
+    header("location:index.php?mensaje=", $mensaje);
+}
+include("../../templates/header.php");
+
+?>
+
+<div class="card">
+    <div class="card-header">
+        Crear Configuracíon
+    </div>
+    <div class="card-body">
+        <form action="" enctype="multipart/form-data" method="post">
+
+            <div class="mb-3">
+                <label for="nombreconfiguracion" class="form-label">Nombre Configuraciónes:</label>
+                <input type="text" class="form-control" name="nombreconfiguracion" id="nombreconfiguracion" aria-describedby="helpId" placeholder="Nombre Configuraciónes">
+
+            </div>
+
+            <div class="mb-3">
+                <label for="valor" class="form-label">Valor:</label>
+                <input type="text" class="form-control" name="valor" id="valor" aria-describedby="helpId" placeholder="Valor">
+
+            </div>
+
+            <button type="submit" class="btn btn-success">Guardar</button>
+            <a name="" id="" class="btn btn-primary" href="index.php" role="button">Cancelar</a>
+
+        </form>}
+    </div>
+    <div class="card-footer text-muted">
+
+    </div>
+</div>
+<?php include("../../templates/footer.php"); ?>
--- admin/secciones/configuraciones/index.php
+++ admin/secciones/configuraciones/index.php
@@ -1,3 +1,52 @@
-<?php include("../../templates/header.php");?>
-Listar Configuracíon 
-<?php include("../../templates/footer.php");?>
+<?php
+include("../../bd.php");
+
+//seleccionar registros
+$sentencia = $conexion->prepare("SELECT * FROM `configuraciones` ");
+$sentencia->execute();
+$lista_configuraciones = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+
+include("../../templates/header.php");
+
+?>
+Listar Configuracíon
+<div class="card">
+    <div class="card-header">
+        <a name="" id="" class="btn btn-primary" href="crear.php" role="button">Crear Configuraciones </a>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive-sm">
+            <table class="table ">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Nombre de la Configración </th>
+                        <th scope="col">valor</th>
+                        <th scope="col">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($lista_configuraciones as $registros) { ?>
+                        <tr class="">
+                            <td scope="row">R1C1</td>
+                            <td>R1C2</td>
+                            <td>R1C3</td>
+                            <td>Editar/eliminar</td>
+                        </tr>
+                        <tr class="">
+                            <td scope="row">Item</td>
+                            <td>Item</td>
+                            <td>Item</td>
+
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+    <div class="card-footer text-muted">
+
+    </div>
+</div>
+<?php include("../../templates/footer.php"); ?>
